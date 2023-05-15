df6b2814ba1b6abc1c877979d2b5b46e447c338b
Thangam N <thangam.nainar0507@gmail.com>
2023-05-15 11:36:31 +0530

    changes saved

---
+++ client-side/index.js
@@ -0,0 +1,128 @@
+// get the navbar element
+const navbar = document.querySelector('nav');
+
+// listen for scroll events
+window.addEventListener('scroll', () => {
+  // get the current scroll position
+  const scrollPosition = window.scrollY;
+
+  // set the background color of the navbar based on the scroll position
+  if (scrollPosition > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+// form validation
+
+function validateForm() {
+  // Get form inputs
+
+
+  return true;
+}
+
+document.getElementById("submit-btn").addEventListener("click", function (event) {
+
+  event.preventDefault(); // Prevent form submission
+
+  var firstName = document.getElementById("inputFirstName");
+  var lastName = document.getElementById("inputLastName");
+  var email = document.getElementById("inputMail");
+  var phone = document.getElementById("phoneNumber");
+  var message = document.getElementById("message");
+
+  // Validate first name
+  if (firstName.value == "") {
+    alert("Please enter your first name.");
+    firstName.focus();
+    return false;
+  }
+
+  // Validate last name
+  if (lastName.value == "") {
+    alert("Please enter your last name.");
+    lastName.focus();
+    return false;
+  }
+
+  // Validate email
+  if (email.value == "") {
+    alert("Please enter your email.");
+    email.focus();
+    return false;
+  } else {
+    // Check email format
+    var emailRegex = /^\S+@\S+\.\S+$/;
+    if (!emailRegex.test(email.value)) {
+      alert("Please enter a valid email.");
+      email.focus();
+      return false;
+    }
+  }
+
+  // Validate phone number
+  if (phone.value == "") {
+    alert("Please enter your phone number.");
+    phone.focus();
+    return false;
+  } else {
+    // Check phone number format
+    var phoneRegex = /\d{1,3} \d{3} \d{2} \d{2}$/;
+    if (!phoneRegex.test(phone.value)) {
+      alert("Please enter a valid phone number (e.g. +123 456 78 90).");
+      phone.focus();
+      return false;
+    }
+  }
+
+  // Validate message
+  if (message.value == "") {
+    alert("Please enter a message.");
+    message.focus();
+    return false;
+  }
+
+
+  // Hide form fields
+
+  document.getElementById('myForm').style.display = 'none';
+
+  document.getElementById("thankYouMessage").style.display = "block";
+
+});
+
+
+
+// function CreateUser() {
+//   // post data
+//   fetch('http://localhost:3000/insert', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       first_name: document.getElementById('inputFirstName').value,
+//       last_name: document.getElementById('inputLastName').value,
+//       email: document.getElementById('inputMail').value,
+//       phone_number: document.getElementById('phoneNumber').value,
+//       message: document.getElementById('message').value,
+//     }),
+//     headers: {
+//       'Content-type': 'application/json; charset=UTF-8',
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((json) => {
+//       console.log(json);
+//       const form = document.getElementById('myForm');
+//       form.reset();
+//     })
+//     .catch((error) => {
+//       console.error('Error:', error);
+//     });
+// }
+
+
+
+
+
+
---
+++ server-side/index.js
@@ -0,0 +1,36 @@
+let express=require('express');
+let mysql=require('mysql');
+let cors=require('cors');
+// const { log } = require('console');
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+var connection=mysql.createConnection({
+    host:'127.0.0.1',
+    port:'3306',
+    user:'thangam',
+    password:'Thasan24',
+    database:'portfolio'
+});
+
+connection.connect();
+
+
+app.post('/insert', (req, res) => {
+    const sql = 'INSERT INTO details (first_name, last_name, email, phone_number, message) VALUES (?, ?, ?, ?, ?)';
+    let { first_name, last_name ,email, phone_number, message } = req.body;
+    connection.query(sql,[first_name ,last_name, email, phone_number, message] ,function (error, results) {
+        if (error) {
+            console.log('error ', error);
+        } else {
+            console.log('result is ', JSON.stringify(results));
+        }
+        res.send(JSON.stringify(results));
+        // connection.end();
+    });
+});
+
+app.listen(3000, ()=>{
+    console.log('server is running ');
+});
